03ee2410cef15a0fed959019c7fc8a5d7e32ebe9
Max <186575394+maksme@users.noreply.github.com>
2024-11-25 15:03:46 +0200

    1.1

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
-const mainElement = document.querySelector(".main");
+const mainEl = document.querySelector(".main");
 
+function createNote(title, text) {
+  const noteEl = document.createElement("div");
+  noteEl.classList.add("note");
+  noteEl.innerHTML = `
+    <div class="note-header">
+      <p>${title}</p>
+      <div class="note-actions">
+        <button class="note-edit">
+          <img src="/img/edit.svg" alt="Edit button" />
+        </button>
+        <button class="note-delete">
+          <img src="/img/delete.svg" alt="Delete button" />
+        </button>
+      </div>
+    </div>
+    <p id="note-description">${text}</p>
+    `;
+}
