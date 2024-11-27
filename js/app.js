8462cee1cc01406ea698ff45925b66ac1b59ef56
Max <186575394+maksme@users.noreply.github.com>
2024-11-27 14:12:59 +0200

    1.2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-const mainEl = document.querySelector(".main");
+const notesEl = document.querySelector(".notes");
+const addButton = document.querySelector(".note-add");
 
+// Create Note Function
 function createNote(title, text) {
   const noteEl = document.createElement("div");
   noteEl.classList.add("note");
@@ -7,14 +9,28 @@ function createNote(title, text) {
     <div class="note-header">
       <p>${title}</p>
       <div class="note-actions">
-        <button class="note-edit">
-          <img src="/img/edit.svg" alt="Edit button" />
+        <button class="note-edit button">
+          <img src="/img/edit.svg" alt="Edit button" class="button-img"/>
         </button>
-        <button class="note-delete">
-          <img src="/img/delete.svg" alt="Delete button" />
+        <button class="note-delete button">
+          <img src="/img/delete.svg" alt="Delete button" class="button-img"/>
         </button>
       </div>
     </div>
     <p id="note-description">${text}</p>
     `;
+  const editBtn = noteEl.querySelector(".note-edit");
+  const deleteBtn = noteEl.querySelector(".note-delete");
+  // Edit note
+  // editBtn.addEventListener("click", (e) => {});
+  // Delete note
+  deleteBtn.addEventListener("click", (e) => {
+    noteEl.remove(e);
+  });
+  return noteEl;
 }
+
+addButton.addEventListener("click", (e) => {
+  const el = createNote("Header", "Your text");
+  notesEl.appendChild(el);
+});
