4ae3f13fd0580142f897f26d9b640fbb8222fd62
Max <186575394+mxbv@users.noreply.github.com>
2025-01-22 13:54:59 +0200

    5.0

--- app.js
+++ app.js
@@ -30,8 +30,19 @@ function createNoteElement(noteData) {
         noteData.title || ""
       }</textarea>
       <button class="note-delete" title="Delete">
-        <svg class="cf-icon-svg" width="35px" height="35px" fill="#000000" viewBox="-3.5 0 19 19" xmlns="http://www.w3.org/2000/svg">
-          <path d="M11.383 13.644A1.03 1.03 0 0 1 9.928 15.1L6 11.172 2.072 15.1a1.03 1.03 0 1 1-1.455-1.456l3.928-3.928L.617 5.79a1.03 1.03 0 1 1 1.455-1.456L6 8.261l3.928-3.928a1.03 1.03 0 0 1 1.455 1.456L7.455 9.716z"/>
+        <svg
+          width="35px"
+          height="35px"
+          fill="none"
+          stroke="#000000"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          >
+          <line x1="18" y1="6" x2="6" y2="18" />
+          <line x1="6" y1="6" x2="18" y2="18" />
         </svg>
 
       </button>
